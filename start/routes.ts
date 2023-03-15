78d8b3ee508a96d6a1a80fc4c6d4f55e568dfea7
FelipeToledo <felipetoledo88@gmail.com>
2023-03-14 22:29:42 -0300

    feat: liberando cors

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,6 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.get('hello', async () => {
+  return { teste: 'hello-world' }
+}).prefix('/api')
