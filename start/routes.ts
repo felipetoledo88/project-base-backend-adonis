409d5149b212e47bb3c9ddf012475fe87fc74a9d
FelipeToledo <felipetoledo.nextage@gmail.com>
2023-07-02 17:56:34 -0300

    feat: permitindo cors

--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,10 @@
 import Route from '@ioc:Adonis/Core/Route'
 import Database from '@ioc:Adonis/Lucid/Database'
 
-Route.get('hello', async () => {
-  return Database.from('users').select('*')
+Route.group(() => {
+  Route.get('users', async () => {
+    return Database.from('users').select('*')
+  })
+  Route.post('login', 'AuthController.login')
+  Route.post('logout', 'AuthController.logout')
 }).prefix('api')
-
-Route.post('login', 'AuthController.login')
-Route.post('logout', 'AuthController.logout')
